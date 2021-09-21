6047f91b2697a1375e71d692312f3bf10d186eff
skeanster <ryanskeans@gmail.com>
2021-09-21 16:59:53 -0700

    calculator working

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
-add = (num1, num2) => {return num1 + num2}
-subtract = (num1, num2) => {return num1 - num2}
-multilply = (num1, num2) => {return num1*num2}
-divide = (num1, num2) => {return num1/num2}
+const clear = document.querySelector('#clear')
+clear.onclick =() => {bottomScreen.textContent = ''; topScreen.textContent=''}
 
 const functionBtns = document.querySelectorAll("#functionBtns")
 const topScreen = document.querySelector("#topScreen")
@@ -18,16 +16,34 @@ const two = document.querySelector('#two')
 const three = document.querySelector('#three')
 const zero = document.querySelector('#zero')
 
-seven.onclick = (num) => {bottomScreen.textContent= bottomScreen.textContent + '7'}
-eight.onclick = (num) => {bottomScreen.textContent= bottomScreen.textContent + '8'}
-nine.onclick = (num) => {bottomScreen.textContent= bottomScreen.textContent + '9'}
-four.onclick = (num) => {bottomScreen.textContent= bottomScreen.textContent + '4'}
-five.onclick = (num) => {bottomScreen.textContent= bottomScreen.textContent + '5'}
-six.onclick = (num) => {bottomScreen.textContent= bottomScreen.textContent + '6'}
-one.onclick = (num) => {bottomScreen.textContent= bottomScreen.textContent + '1'}
-two.onclick = (num) => {bottomScreen.textContent= bottomScreen.textContent + '2'}
-three.onclick = (num) => {bottomScreen.textContent= bottomScreen.textContent + '3'}
-zero.onclick = (num) => {bottomScreen.textContent= bottomScreen.textContent + '0'}
+seven.onclick = (num) => {bottomScreen.textContent= bottomScreen.textContent + 7}
+eight.onclick = (num) => {bottomScreen.textContent= bottomScreen.textContent + 8}
+nine.onclick = (num) => {bottomScreen.textContent= bottomScreen.textContent + 9}
+four.onclick = (num) => {bottomScreen.textContent= bottomScreen.textContent + 4}
+five.onclick = (num) => {bottomScreen.textContent= bottomScreen.textContent + 5}
+six.onclick = (num) => {bottomScreen.textContent= bottomScreen.textContent + 6}
+one.onclick = (num) => {bottomScreen.textContent= bottomScreen.textContent + 1}
+two.onclick = (num) => {bottomScreen.textContent= bottomScreen.textContent + 2}
+three.onclick = (num) => {bottomScreen.textContent= bottomScreen.textContent + 3}
+zero.onclick = (num) => {bottomScreen.textContent= bottomScreen.textContent + 0}
 
+const division = document.querySelector('#divide')
+const times = document.querySelector('#times')
+const minus = document.querySelector('#minus')
+const plus = document.querySelector('#plus')
+const equals = document.querySelector('#equals')
 
+plus.onclick = () => {topScreen.textContent=bottomScreen.textContent + ' +'; bottomScreen.textContent=''}
+minus.onclick = () => {topScreen.textContent=bottomScreen.textContent + ' -'; bottomScreen.textContent=''}
+times.onclick = () => {topScreen.textContent=bottomScreen.textContent + ' x'; bottomScreen.textContent=''}
+division.onclick = () => {topScreen.textContent=bottomScreen.textContent + ' /'; bottomScreen.textContent=''}
 
+equals.onclick = () => {let string = topScreen.textContent + ' ' + bottomScreen.textContent; 
+topScreen.textContent=topScreen.textContent+ ' ' + bottomScreen.textContent;
+string = string.split(' ');
+console.log(string[1])
+if (string[1] == '+') {bottomScreen.textContent= (+string[0] + +string[2])}
+else if (string[1] == '-') {bottomScreen.textContent= (+string[0] - +string[2])}
+else if (string[1] == 'x') {bottomScreen.textContent= (+string[0] * +string[2])}
+else if (string[1] == '/') {bottomScreen.textContent= (+string[0] / +string[2])}
+}
