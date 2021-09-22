d8e9df22e3ecba8c7e2e339c02276b6a93b10d6a
skeanster <ryanskeans@gmail.com>
2021-09-22 12:48:41 -0700

    fixed equals sign bug

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ seven.onclick = (num) => {bottomScreen.textContent= bottomScreen.textContent + 7
 eight.onclick = (num) => {bottomScreen.textContent= bottomScreen.textContent + 8}
 nine.onclick = (num) => {bottomScreen.textContent= bottomScreen.textContent + 9}
 four.onclick = (num) => {bottomScreen.textContent= bottomScreen.textContent + 4}
-five.onclick = (num) => {bottomScreen.textContent= bottomScreen.textContent + 5}
+five.onclick = (num) => {bottomScreen.textContent= bottomScreen.textContent + 5; console.log(bottomScreen.textContent)}
 six.onclick = (num) => {bottomScreen.textContent= bottomScreen.textContent + 6}
 one.onclick = (num) => {bottomScreen.textContent= bottomScreen.textContent + 1}
 two.onclick = (num) => {bottomScreen.textContent= bottomScreen.textContent + 2}
@@ -37,17 +37,56 @@ const minus = document.querySelector('#minus')
 const plus = document.querySelector('#plus')
 const equals = document.querySelector('#equals')
 
-plus.onclick = () => {topScreen.textContent=bottomScreen.textContent + ' +'; bottomScreen.textContent=''}
-minus.onclick = () => {topScreen.textContent=bottomScreen.textContent + ' -'; bottomScreen.textContent=''}
-times.onclick = () => {topScreen.textContent=bottomScreen.textContent + ' x'; bottomScreen.textContent=''}
-division.onclick = () => {topScreen.textContent=bottomScreen.textContent + ' /'; bottomScreen.textContent=''}
 
 equals.onclick = () => {let string = topScreen.textContent + ' ' + bottomScreen.textContent; 
-topScreen.textContent=topScreen.textContent+ ' ' + bottomScreen.textContent;
 string = string.split(' ');
-console.log(string[1])
 if (string[1] == '+') {bottomScreen.textContent= (+string[0] + +string[2])}
 else if (string[1] == '-') {bottomScreen.textContent= (+string[0] - +string[2])}
 else if (string[1] == 'x') {bottomScreen.textContent= (+string[0] * +string[2])}
 else if (string[1] == '/') {bottomScreen.textContent= (+string[0] / +string[2])}
+topScreen.textContent=''
+}
+
+plus.onclick = () => {let string = topScreen.textContent + ' ' + bottomScreen.textContent; 
+string = string.split(' ');
+if (string[1] == '+') {bottomScreen.textContent= (+string[0] + +string[2])}
+else if (string[1] == '-') {bottomScreen.textContent= (+string[0] - +string[2])}
+else if (string[1] == 'x') {bottomScreen.textContent= (+string[0] * +string[2])}
+else if (string[1] == '/') {bottomScreen.textContent= (+string[0] / +string[2])}
+
+topScreen.textContent=bottomScreen.textContent + ' +'
+bottomScreen.textContent = ''
+}
+
+minus.onclick = () => {let string = topScreen.textContent + ' ' + bottomScreen.textContent; 
+string = string.split(' ');
+if (string[1] == '+') {bottomScreen.textContent= (+string[0] + +string[2])}
+else if (string[1] == '-') {bottomScreen.textContent= (+string[0] - +string[2])}
+else if (string[1] == 'x') {bottomScreen.textContent= (+string[0] * +string[2])}
+else if (string[1] == '/') {bottomScreen.textContent= (+string[0] / +string[2])}
+
+topScreen.textContent=bottomScreen.textContent + ' -'
+bottomScreen.textContent = ''  
+}
+
+times.onclick = () => {let string = topScreen.textContent + ' ' + bottomScreen.textContent; 
+string = string.split(' ');
+if (string[1] == '+') {bottomScreen.textContent= (+string[0] + +string[2])}
+else if (string[1] == '-') {bottomScreen.textContent= (+string[0] - +string[2])}
+else if (string[1] == 'x') {bottomScreen.textContent= (+string[0] * +string[2])}
+else if (string[1] == '/') {bottomScreen.textContent= (+string[0] / +string[2])}
+
+topScreen.textContent=bottomScreen.textContent + ' x'
+bottomScreen.textContent = ''  
+}
+
+division.onclick = () => {let string = topScreen.textContent + ' ' + bottomScreen.textContent; 
+string = string.split(' ');
+if (string[1] == '+') {bottomScreen.textContent= (+string[0] + +string[2])}
+else if (string[1] == '-') {bottomScreen.textContent= (+string[0] - +string[2])}
+else if (string[1] == 'x') {bottomScreen.textContent= (+string[0] * +string[2])}
+else if (string[1] == '/') {bottomScreen.textContent= (+string[0] / +string[2])}
+
+topScreen.textContent=bottomScreen.textContent + ' /'
+bottomScreen.textContent = ''  
 }
